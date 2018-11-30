c430c336a8aa51d676c259cce63e8b962ab18175
李城（大橙子） <1048331019@qq.com>
2018-11-30 19:19:31 +0800

    Add files via upload
    利用ceres solver 完成2D-3D相机位姿的估计，后续会附上自己的源码，和opencv solvepnp求解的对比！

---
+++ cameraPose/build/CMakeFiles/campose.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/campose.dir/main.cpp.o"
+  "campose.pdb"
+  "campose"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/campose.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
